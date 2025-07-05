8e8e9d083c5e553b363ce6a075093782485dd159
samir <samirbrahma2003@gmail.com>
2025-07-05 10:53:10 +0530

    commit

--- server/routes/payment.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createCashfreeOrder, createPaymentIntent } = require('../controllers/paymentController');
-
-router.post('/create-cashfree-order', createCashfreeOrder);
-router.post('/create-payment-intent', createPaymentIntent);
-
-module.exports = router; 
